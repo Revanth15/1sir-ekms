b9b3f0f134e1dd3dbdca3c1b9687c4bde4cabf97
Revanth <revanth1509@gmail.com>
2025-08-20 12:05:04 +0800

    test updated

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,49 @@
 
 import LoadingScreen from "@/components/loading";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import type ScanbotSDK from "scanbot-web-sdk/ui";
 
 export default function Home() {
   const router = useRouter()
+  let ScanbotSdk: typeof ScanbotSDK;
+  const [scanResult, setScanResult] = useState("");
 
   useEffect(() => {
-      router.push("/scan")
-  }, [router])
+    loadSDK();
+  });
+
+  async function loadSDK() {
+    ScanbotSdk = (await import('scanbot-web-sdk/ui')).default;
+
+    let licencekey = process.env.LICENCEKEY || ""
+    await ScanbotSdk.initialize({
+      licenseKey: licencekey,
+      enginePath: "/wasm/",
+    });
+  }
+
+  async function startBarcodeScanner() {
+    const config = new ScanbotSdk.UI.Config.BarcodeScannerScreenConfiguration();
+    const result = await ScanbotSdk.UI.createBarcodeScanner(config);
+
+    if (result && result.items.length > 0) {
+      setScanResult(result.items[0].barcode.text);
+    }
+  }
+
+  // initialize the Scanbot Barcode SDK
+  
+
+  // useEffect(() => {
+  //     router.push("/scan")
+  // }, [router])
 
   return (
     <div className="bg-neutral-800 min-h-screen">
-      <LoadingScreen/>
+      {/* <LoadingScreen/> */}
+      <button onClick={startBarcodeScanner}>Scan Barcodes</button>
+      <p>{scanResult}</p>
     </div>
   );
 }
